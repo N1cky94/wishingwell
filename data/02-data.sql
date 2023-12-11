6ffc8ee54f5f4b0903ce816f08c7f216463ba8b1
Nicky <nick.bauters@me.com>
2023-12-11 19:20:33 +0100

    init:fix: Change name field to firstname in the users table and fix colon to semicolon at the end of document

--- data/02-data.sql
+++ data/02-data.sql
@@ -1,4 +1,4 @@
-insert into users (name) values
+insert into users (firstname) values
     ('Nick'),
     ('Kelly'),
     ('Fynn'),
@@ -8,6 +8,6 @@ insert into users (name) values
 
 insert into authentication_users
 (email, password, user_id, role) values
-    ('nick@me.com', 'psw', 1, 'ADMIN'),
-    ('kelly@me.com', 'psw', 2, 'USER')
-:
+    ('nick@me.com', '$2a$12$5qkEsAkQ2aFA/dWNnh61x.jbw38NeeC6SqyncHzRYnZBwOMixpfQ6', 1, 'ADMIN'),
+    ('kelly@me.com', '$2a$12$5qkEsAkQ2aFA/dWNnh61x.jbw38NeeC6SqyncHzRYnZBwOMixpfQ6', 2, 'USER')
+;
